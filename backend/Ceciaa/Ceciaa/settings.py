269edd36126d9195c57d36ecbe812d4b41c27ba9
Bourdon94m <matthieu.pro94@gmail.com>
2024-07-30 14:05:48 +0200

    after migrate models in Teamviewer.models (Client)

--- backend/Ceciaa/Ceciaa/settings.py
+++ backend/Ceciaa/Ceciaa/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'authentication', # Apps auth
+    'Teamviewer', #Apps Client Teamviewer
     'rest_framework', # API 
 ]
 
