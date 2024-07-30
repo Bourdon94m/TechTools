a1dde3c5dda5547aed47f4e4bb35dfb0f419c9f1
Bourdon94m <matthieu.pro94@gmail.com>
2024-07-30 14:25:38 +0200

    added endpoint for get all client in Teamviewers apps

--- backend/Ceciaa/Teamviewer/views.py
+++ backend/Ceciaa/Teamviewer/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
-
+from rest_framework import generics
+from .models import Client
+from .serializers import ClientSerializers
 # Create your views here.
+
+# CBV (Class-Based Views)
+
+# Voir tous les users
+class ClientListCreate(generics.ListAPIView):
+    # Définit l'ensemble des objets à retourner (tous les CustomUser)
+    queryset = Client.objects.all()
+    # Spécifie le sérialiseur à utiliser pour formater les données
+    serializer_class = ClientSerializers
