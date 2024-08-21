28fd5b39750a1fafa219184fef77032d3d3d20f0
Bourdon94m <matthieu.pro94@gmail.com>
2024-08-21 20:52:52 +0200

    Add new chart graph components

--- frontend/Tech/src/components/layout/Dashboard/ChartCards.jsx
+++ frontend/Tech/src/components/layout/Dashboard/ChartCards.jsx
@@ -3,15 +3,24 @@ import {
   Bar,
   BarChart,
   CartesianGrid,
+  LabelList,
   XAxis,
+  YAxis,
+  Line,
   ResponsiveContainer,
+  LineChart,
+  AreaChart,
+  Area,
 } from "recharts";
+import { TrendingUp } from "lucide-react";
+
 import {
   Card,
   CardContent,
   CardDescription,
   CardHeader,
   CardTitle,
+  CardFooter,
 } from "@/components/ui/card";
 import {
   ChartContainer,
@@ -33,11 +42,14 @@ const chartData = [
 const chartConfig = {
   desktop: {
     label: "Desktop",
-    color: "hsl(var(--chart-1))",
+    color: "hsl(var(--chart-2))",
+  },
+  label: {
+    color: "hsl(var(--background))",
   },
 };
 
-export function ChartCard({ className, classContainerName, title }) {
+export function BarChartCard({ className, classContainerName, title }) {
   return (
     <Card className={cn("overflow-hidden", className)}>
       <CardHeader className="p-4">
@@ -49,31 +61,185 @@ export function ChartCard({ className, classContainerName, title }) {
           className={cn("w-full", classContainerName)}
           config={chartConfig}
         >
-          <ResponsiveContainer width="100%" height="100%">
+          <ResponsiveContainer width="100%" height={300}>
             <BarChart
               data={chartData}
-              margin={{ top: 10, right: 10, left: -20, bottom: 0 }}
+              layout="vertical"
+              margin={{
+                top: 10,
+                right: 16,
+                left: 0,
+                bottom: 0,
+              }}
             >
-              <CartesianGrid vertical={false} strokeDasharray="3 3" />
+              <CartesianGrid horizontal={false} />
+              <YAxis
+                dataKey="month"
+                type="category"
+                tickLine={false}
+                tickMargin={10}
+                axisLine={false}
+                tickFormatter={(value) => value.slice(0, 3)}
+                hide
+              />
+              <XAxis dataKey="desktop" type="number" hide />
+              <ChartTooltip
+                cursor={false}
+                content={<ChartTooltipContent indicator="line" />}
+              />
+              <Bar dataKey="desktop" fill="var(--color-desktop)" radius={4}>
+                <LabelList
+                  dataKey="month"
+                  position="insideLeft"
+                  offset={8}
+                  className="fill-[--color-label]"
+                  fontSize={12}
+                />
+                <LabelList
+                  dataKey="desktop"
+                  position="right"
+                  offset={8}
+                  className="fill-foreground"
+                  fontSize={12}
+                />
+              </Bar>
+            </BarChart>
+          </ResponsiveContainer>
+        </ChartContainer>
+      </CardContent>
+      <CardFooter className="flex-col items-start gap-2 text-sm mt-10">
+        <div className="flex gap-2 font-medium leading-none">
+          Trending up by 5.2% this month <TrendingUp className="h-4 w-4" />
+        </div>
+        <div className="leading-none text-muted-foreground">
+          Showing total visitors for the last 6 months
+        </div>
+      </CardFooter>
+    </Card>
+  );
+}
+
+export function LineChartCard({ className, classContainerName, title }) {
+  return (
+    <Card className={cn("overflow-hidden", className)}>
+      <CardHeader className="p-4">
+        <CardTitle className="text-lg bg-">{title}</CardTitle>
+        <CardDescription>January - June 2024</CardDescription>
+      </CardHeader>
+      <CardContent className="p-0">
+        <ChartContainer
+          className={cn("w-full", classContainerName)}
+          config={chartConfig}
+        >
+          <ResponsiveContainer width="100%" height={300}>
+            <LineChart
+              data={chartData}
+              margin={{
+                top: 10,
+                right: 12,
+                left: 12,
+                bottom: 0,
+              }}
+            >
+              <CartesianGrid vertical={false} />
               <XAxis
                 dataKey="month"
                 tickLine={false}
                 axisLine={false}
-                tick={{ fontSize: 12 }}
+                tickMargin={8}
+                tickFormatter={(value) => value.slice(0, 3)}
               />
               <ChartTooltip
                 cursor={false}
                 content={<ChartTooltipContent hideLabel />}
               />
-              <Bar
+              <Line
+                type="natural"
                 dataKey="desktop"
-                fill="var(--color-desktop)"
-                radius={[4, 4, 0, 0]}
+                stroke="var(--color-desktop)"
+                strokeWidth={2}
+                dot={{
+                  fill: "var(--color-desktop)",
+                }}
+                activeDot={{
+                  r: 6,
+                }}
               />
-            </BarChart>
+            </LineChart>
           </ResponsiveContainer>
         </ChartContainer>
       </CardContent>
+      <CardFooter className="flex-col items-start gap-2 text-sm mt-10">
+        <div className="flex gap-2 font-medium leading-none">
+          Trending up by 5.2% this month <TrendingUp className="h-4 w-4" />
+        </div>
+        <div className="leading-none text-muted-foreground">
+          Showing total visitors for the last 6 months
+        </div>
+      </CardFooter>
+    </Card>
+  );
+}
+
+export function AreaChartCard({ className, classContainerName, title }) {
+  return (
+    <Card className={cn("overflow-hidden", className)}>
+      <CardHeader className="p-4">
+        <CardTitle className="text-lg">{title}</CardTitle>
+        <CardDescription>
+          Showing total visitors for the last 6 months
+        </CardDescription>
+      </CardHeader>
+      <CardContent className="p-0">
+        <ChartContainer
+          className={cn("w-full", classContainerName)}
+          config={chartConfig}
+        >
+          <ResponsiveContainer width="100%" height={300}>
+            <AreaChart
+              data={chartData}
+              margin={{
+                top: 10,
+                right: 12,
+                left: 12,
+                bottom: 0,
+              }}
+            >
+              <CartesianGrid vertical={false} />
+              <XAxis
+                dataKey="month"
+                tickLine={false}
+                axisLine={false}
+                tickMargin={8}
+                tickFormatter={(value) => value.slice(0, 3)}
+              />
+              <ChartTooltip
+                cursor={false}
+                content={<ChartTooltipContent indicator="dot" hideLabel />}
+              />
+              <Area
+                type="linear"
+                dataKey="desktop"
+                fill="var(--color-desktop)"
+                fillOpacity={0.4}
+                stroke="var(--color-desktop)"
+              />
+            </AreaChart>
+          </ResponsiveContainer>
+        </ChartContainer>
+      </CardContent>
+      <CardFooter>
+        <div className="flex w-full items-start gap-2 text-sm mt-10">
+          <div className="grid gap-2">
+            <div className="flex items-center gap-2 font-medium leading-none">
+              Trending up by 5.2% this month <TrendingUp className="h-4 w-4" />
+            </div>
+            <div className="flex items-center gap-2 leading-none text-muted-foreground">
+              January - June 2024
+            </div>
+          </div>
+        </div>
+      </CardFooter>
     </Card>
   );
 }
